9eddb6db55c4d988cb6ae749777059d132b7b263
Kalashinikov1 <151412364+Kalashinikov1@users.noreply.github.com>
2023-11-18 21:56:39 -0300

    Add files via upload

---
+++ KalashLoader.lua
@@ -0,0 +1,26 @@
+if game.PlaceId == 14565278851 then
+    print("Anime Skill Fighting !")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Kalashinikov1/Owner/main/AnimeSkill.lua"))()
+elseif game.PlaceId == 15084364495 then
+    print("Bike Race Simulatort")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Kalashinikov1/Owner/main/Bicicleta.lua"))()
+elseif game.PlaceId == 15107361788 then
+    print("Toilet Fighting Simulator")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Kalashinikov1/Owner/main/toiletsimu.lua"))()
+elseif game.PlaceId == 14368340794 then
+    print("Swim Race Simulator")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Kalashinikov1/Owner/main/nadar.lua"))()
+elseif game.PlaceId == 9144187696 then
+    print("OP Magic Simulator")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Kalashinikov1/Owner/main/MagicBoxer.lua"))()
+elseif game.PlaceId == 15117193943 then
+    print("Snowboard Race Simulator")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Kalashinikov1/Owner/main/snowboard.lua"))()
+elseif game.PlaceId == 14762790937 then
+    print("Anime Waves Simulator")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Kalashinikov1/Owner/main/Ondinha.lua"))()
+elseif game.PlaceId == 13988823284 then
+    print("Fighting Legends")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Kalashinikov1/Owner/main/Espadinha.lua"))()
+else
+end
